2f89e9e3cf302e98cfd80f32c7171b9a9b2820c8
MattNeedsToLearn <mm.kupczyk@gmail.com>
2023-10-05 10:23:43 +0100

    testing webhook

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
     name = getenv('YOUR_NAME')
     if name is None:
       name = "friend"
-    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in {hostname}. Also, Orks iz da best! WAAAAGH!.</h2>\n"
+    return f"<h1>Hello {name}.</h1>\n\n<h2>I'm currently running in da Machine{hostname}. Also, Orks iz da best! WAAAAGH!.</h2>\n"
 
 if __name__=='__main__':
   app.run(host='0.0.0.0', port=5500, debug=True)
